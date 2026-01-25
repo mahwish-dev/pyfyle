ac5acfea3fc8aedcc1613910470a226b85e21914
sid314 <sidaa314@gmail.com>
2026-01-25 13:17:20 +0530

    avoid recreating venvs

--- bin/pyfyle-viewer.sh
+++ bin/pyfyle-viewer.sh
@@ -2,6 +2,8 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd pyfyle || exit
-uv venv
+if ! [ -f ".venv/bin.activate" ]; then
+  uv venv
+fi
 source ".venv/bin/activate"
 uv run "$SCRIPT_DIR/../main.py" "$@"

da91105bdaff75d230c8a6c8ef010cbbce08e57b
sid314 <sidaa314@gmail.com>
2026-01-25 13:04:30 +0530

    more debugging tui run script

--- bin/pyfyle-viewer.sh
+++ bin/pyfyle-viewer.sh
@@ -2,5 +2,9 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pwd
+cd "$SCRIPT_DIR/pyfyle" || exit
+uv venv
+source "$SCRIPT_DIR/pyfyle/.venv/bin/activate"
+cd ..
 
 uv run "$SCRIPT_DIR/../main.py" "$@"

2b757127e85d43f5797dc8340c9b9fa782a27a1a
sid314 <sidaa314@gmail.com>
2026-01-25 13:13:26 +0530

    remove unused code

--- bin/pyfyle-viewer.sh
+++ bin/pyfyle-viewer.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-pwd
 cd pyfyle || exit
-pwd
 uv venv
 source ".venv/bin/activate"
-echo "$@"
 uv run "$SCRIPT_DIR/../main.py" "$@"
